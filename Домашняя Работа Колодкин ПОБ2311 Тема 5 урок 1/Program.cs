e23f08da428bc726b5da97b168b19ca2e7890d9a
Пользователь <@DESKTOP-2GPIOM1>
2023-09-11 01:39:19 +0300

    Добавьте файлы проекта.

---
+++ Домашняя Работа Колодкин ПОБ2311 Тема 5 урок 1/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача (а)
+////миллиметр в сантиметры
+//const float sm = 0.1f;
+//Console.Write("Введите миллиметры:");
+//double mm = double.Parse(Console.ReadLine());
+//double res = sm * mm;
+//Console.WriteLine($"{mm} миллиметры {res:F2} сантиметров");
+
+//Задача (б)
+//const float m = 0.001f;
+//Console.WriteLine("Введите Миллиметры:");
+//double mm=double.Parse(Console.ReadLine());
+//double res = m * mm;
+//Console.WriteLine($"{m} миллиметры {res:F2} метров");
+
+//Задача (в)
+//const float km = 0.0001f;
+//Console.WriteLine("Введите Миллиметры:");
+//double mm=double.Parse(Console.ReadLine());
+//double res = km * mm;
+//Console.WriteLine($"{km} миллиметров {res:F2} киллометров");
+
+//Задача (г)
+//const float ml = 0.00001f;
+//Console.WriteLine("Введите Миллиметры");
+//double mm = double.Parse(Console.ReadLine());
+//double res = ml * mm;
+//Console.WriteLine($"{ml} миллиметров {res:F4} миль");
+
+////Задача (д)
+//const float yards = 0.00111f;
+//Console.WriteLine("Введите Миллиметры");
+//double mm=double.Parse(Console.ReadLine());
+//double res = yards * mm;
+//Console.WriteLine($"{yards} миллиметров {res:F6} ярдов:");
+
+
+
